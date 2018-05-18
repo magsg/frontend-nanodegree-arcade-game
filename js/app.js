163640dd0c28366fc1aa8b1af0284799eb640150
Malgorzata <mw.gradowska@gmail.com>
2018-05-18 15:54:15 +0100

    puts ticks on canvas

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -27,7 +31,13 @@ Enemy.prototype.render = function() {
 
 
 // Now instantiate your objects.
+let tickA = new  Enemy(10,150);
+let tickB = new  Enemy(10,250);
+let tickC = new  Enemy(10,350);
+let tickD = new  Enemy(10,450);
 // Place all enemy objects in an array called allEnemies
+const allEnemies = [tickA, tickB, tickC, tickD];
+
 // Place the player object in a variable called player
 
 
